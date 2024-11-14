feb2378b1ed01bd46bae21e6bbc4b3bb56b9b636
Eugene Popov <kosmokot45@gmail.com>
2024-11-14 15:27:47 +0300

    dev add swagger/redoc

--- fur_store/fur_store/urls.py
+++ fur_store/fur_store/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api_v1/", include("shop.urls")),  # Включение маршрутов из приложения shop
+    path("api_v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api_v1/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api_v1/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
