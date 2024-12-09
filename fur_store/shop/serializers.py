b376652adbc69460852cd866fb6a97275b8b6a35
Eugene Popov <kosmokot45@gmail.com>
2024-12-10 00:12:53 +0300

    dev докоммит

--- fur_store/shop/serializers.py
+++ fur_store/shop/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from .models import Product, Category, Cart, Order
+from .models import Product, Category, Cart, Order, CartItem, OrderItem
+
+# from .models import Cart, CartItem, Order, OrderItem, Product
+
+
+class ProductsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "description", "image", "price", "characteristics"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -14,13 +22,52 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+# class CartSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cart
+#         fields = "__all__"
+
+
+# class OrderSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Order
+#         fields = "__all__"
+
+
+class CategoryIDSerializer(serializers.Serializer):
+    category_id = serializers.IntegerField(required=False)
+
+
+#
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = CartItem
+        fields = ["product", "quantity"]
+
+
 class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(source="cartitem_set", many=True)
+
     class Meta:
         model = Cart
-        fields = "__all__"
+        fields = ["items"]
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ["product", "quantity"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(source="orderitem_set", many=True)
+
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = ["id", "user", "created_at", "items"]
--- fur_store/shop/urls.py
+++ fur_store/shop/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet, CartViewSet, OrderViewSet
+from .views import ProductsViewSet, ProductViewSet, CategoryViewSet, CartViewSet, OrderViewSet
 
 router = DefaultRouter()
-router.register(r"products", ProductViewSet)
-router.register(r"categories", CategoryViewSet)
-router.register(r"cart", CartViewSet)
-router.register(r"order", OrderViewSet)
+router.register(r"products", ProductsViewSet, basename="products")
+router.register(r"product", ProductViewSet, basename="product")
+router.register(r"categories", CategoryViewSet, basename="categories")
+router.register(r"cart", CartViewSet, basename="cart")
+router.register(r"order", OrderViewSet, basename="order")
 
 urlpatterns = [
     path("", include(router.urls)),
